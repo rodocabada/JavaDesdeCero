9e6fb7737f86b5374ad92572e7f546aa41f7ce6e
Rodolfo Cabada <rodo.cabada@gmail.com>
2017-07-01 22:03:26 -0600

    Se versiona practica del video 43 "Polimorfismo y Enlazado Dinamico".

--- PrimerosPasos/src/poo/Empleado.java
+++ PrimerosPasos/src/poo/Empleado.java
@@ -7,14 +7,27 @@ public class Empleado {
 	private String nombre;
 	private double sueldo;
 	private Date fechaAlta;
+	private int id;
+	private static int idSiguiente = 1;
 	
 	public Empleado(String nombre, double sueldo, int anio, int mes, int dia){
 		GregorianCalendar calendario = new GregorianCalendar(anio, mes-1, dia);
 		this.nombre = nombre;
 		this.sueldo = sueldo;
 		this.fechaAlta = calendario.getTime();
+		id = idSiguiente;
+		idSiguiente++;
 	}
 	
+	public void aumentoSueldo(int porcentaje){
+		double aumento = sueldo * porcentaje/100;
+		sueldo+=aumento;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
 	public Empleado(String nombre){
 		this(nombre, 30000, 2000, 01, 01); 
 	}
@@ -27,8 +40,8 @@ public class Empleado {
 		return sueldo;
 	}
 	
-	public void setSueldoMasAumento(double sueldoMasAumento){
-		sueldo+=sueldoMasAumento;
+	public void setAumento(double aumento){
+		sueldo+=aumento;
 	}
 	
 	public Date getFechaAlta(){
--- PrimerosPasos/src/poo/UsoEmpleado.java
+++ PrimerosPasos/src/poo/UsoEmpleado.java
@@ -2,40 +2,24 @@ package poo;
 
 public class UsoEmpleado {
 	public static void main(String[] args) {
-		/*aumentoSueldo(empleado1, 5);
-		aumentoSueldo(empleado2, 5);
-		aumentoSueldo(empleado3, 5);
+
+		Jefatura jefeRecursosHumanos = new Jefatura("Rodolfo Cabada", 55000, 2006,9,25);
+		jefeRecursosHumanos.setIncentivo(2570);
 		
-		System.out.println("Nombre: " + empleado1.getNombre() + " Sueldo: " + empleado1.getSueldo()
-				+ " Fecha de Alta: " + empleado1.getFechaAlta());
-		System.out.println("Nombre: " + empleado2.getNombre() + " Sueldo: " + empleado2.getSueldo()
-		+ " Fecha de Alta: " + empleado2.getFechaAlta());
-		System.out.println("Nombre: " + empleado3.getNombre() + " Sueldo: " + empleado3.getSueldo()
-		+ " Fecha de Alta: " + empleado3.getFechaAlta());*/
-		
-		Empleado[] empleados = new Empleado[4];
-		empleados[0] = new Empleado("Rodolfo Cabada", 30000, 1990, 12, 17);
+		Empleado[] empleados = new Empleado[6];
+		empleados[0] = new Empleado("Daniel Cabada", 30000, 1990, 12, 17);
 		empleados[1] = new Empleado("Alberto Gonzalez", 10000, 1995, 6, 2);
 		empleados[2] = new Empleado("Hugo Lopez", 25000, 2002, 3, 15);
 		empleados[3] = new Empleado("Alberto Rococo");
-		
-		/*for(int i=0; i<empleados.length; i++){
-			aumentoSueldo(empleados[i], 5);
-			System.out.println("Nombre: " + empleados[i].getNombre() + " Sueldo: " + empleados[i].getSueldo()
-			+ " Fecha de Alta: " + empleados[i].getFechaAlta());
-		}*/
+		empleados[4] = jefeRecursosHumanos; //Polimorfismo en acción. Principio de sustitución.
+		empleados[5] = new Jefatura("Maria Perez", 95000, 1999, 5, 25);
 		
 		for(Empleado fila: empleados){
-			aumentoSueldo(fila, 5);
-			System.out.println("Nombre: " + fila.getNombre() + " Sueldo: " + fila.getSueldo()
+			fila.aumentoSueldo(5);
+			System.out.println("Id: " + fila.getId() + " Nombre: " + fila.getNombre() + " Sueldo: " + fila.getSueldo()
 					+ " Fecha de Alta: " + fila.getFechaAlta());
 		}
 		
-	}
-	
-	public static void aumentoSueldo(Empleado empleado, double porcentaje){
-		double aumento = empleado.getSueldo() * porcentaje/100;
-		empleado.setSueldoMasAumento(aumento);
 	}
 
 }
