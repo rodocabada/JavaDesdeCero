cb93cca71b01080ab33d61828d2ca2b3edca7a35
Rodolfo Cabada <rodo.cabada@gmail.com>
2017-07-10 22:14:15 -0600

    Se versiona practica de video 52.

---
+++ PrimerosPasos/src/poo/PruebaTemporizador.java
@@ -0,0 +1,26 @@
+package poo;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.Timer;
+
+public class PruebaTemporizador {
+
+	public static void main(String[] args) {
+		
+		DameLaHora oyente = new DameLaHora();
+		Timer miTemporizador = new Timer(5000, oyente);
+		miTemporizador.start();
+		JOptionPane.showMessageDialog(null, "Pulsa Aceptar para detener.");
+		System.exit(0);
+
+	}
+
+}
+
+class DameLaHora implements ActionListener{
+	public void actionPerformed(ActionEvent e){
+		Date ahora = new Date();
+		System.out.println("Te pongo la hora cada 5 segundos." + ahora);
+	}
+}
