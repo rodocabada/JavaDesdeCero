4879f6fc97e41e04f72a9cd2aad6365686b2604b
Rodolfo Cabada <rodo.cabada@gmail.com>
2017-07-24 22:10:06 -0600

    Se versionan ultimos cambios

--- PrimerosPasos/src/graficos/PruebaDibujo.java
+++ PrimerosPasos/src/graficos/PruebaDibujo.java
@@ -1,6 +1,8 @@
 package graficos;
 
 import java.awt.*;
+import java.awt.geom.*;
+
 import javax.swing.*;
 
 public class PruebaDibujo {
@@ -33,9 +35,25 @@ class LaminaConFiguras extends JPanel{
 	public void paintComponent(Graphics g){
 		
 		super.paintComponent(g);
-		//g.drawRect(50, 50, 200, 200);
-		//g.drawLine(50, 260, 250, 260);
-		g.drawArc(50, 100, 100, 200, 120, 150);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		Rectangle2D rectangulo = new Rectangle2D.Double(100,100,200,150);
+		g2.draw(rectangulo);
+		
+		Ellipse2D elipse = new Ellipse2D.Double();
+		elipse.setFrame(rectangulo);
+		g2.draw(elipse);
+		
+		g2.draw(new Line2D.Double(100,100,300,250));
+		
+		double centroX = rectangulo.getCenterX();
+		double centroY = rectangulo.getCenterY();
+		double radio = 150;
+		Ellipse2D circulo = new Ellipse2D.Double();
+		
+		circulo.setFrameFromCenter(centroX, centroY, centroX+radio, centroY+radio);
+		g2.draw(circulo);
+		
 		
 	}
 	
